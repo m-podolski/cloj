5ba46bc7f33e433495af1d7edaa234e39fd1b88f
Podolski, Malte <malte.podolski@web.de>
2024-02-10 21:00:53 +0100

    pwg: add validation-functions

--- src/cloj/encryption/pw_generator/validation.clj
+++ src/cloj/encryption/pw_generator/validation.clj
@@ -1,6 +1,22 @@
 (ns cloj.encryption.pw-generator.validation)
 
-(def ratings {:strong 1 :moderate 2 :weak 3})
+
+(defn check-length-complexity [password]
+  {:password          password
+   :length-complexity {:valid      true
+                       :length     20
+                       :complexity 2}})
+
+
+(defn check-surrounding-chars [validation-result]
+  (conj validation-result {:surrounding-chars {:valid true
+                                               :chars []}}))
+
+
+(defn check-repeated-sequences [validation-result]
+  (conj validation-result {:repeated-sequences {:valid     true
+                                                :sequences []}}))
+
 
 (def char-patterns {:alphabetical ["qwertzuiopü" "asdfghjklöä"
                                    "<yxcvbnm,.-" ">YXCVBNM;:_"]
@@ -8,4 +24,20 @@
                     :special      ["!\"§$%&/()=?`" "{[]}\\~"
                                    ",.-#+" ";:_'*" "/*-+"]})
 
-(defn validate [password] {:password password :rating :strong})
+(defn check-char-patterns [validation-result]
+  (conj validation-result {:char-patterns {:valid    true
+                                           :patterns []}}))
+
+
+(defn rate [validation-result]
+  (conj validation-result {:rating   :strong
+                           :problems []}))
+
+
+(defn validate [password]
+  (-> password
+      (check-length-complexity)
+      (check-surrounding-chars)
+      (check-repeated-sequences)
+      (check-char-patterns)
+      (rate)))
--- test/cloj/encryption/pw_generator_test.clj
+++ test/cloj/encryption/pw_generator_test.clj
@@ -5,51 +5,6 @@
     [clojure.test :refer :all]))
 
 
-(deftest test-main
-  (testing "runs"
-    (let [out-str (with-out-str (-main))]
-      (is (= out-str "Invalid Command: No command given")))
-
-    (let [out-str (with-out-str (-main "validate" "mompl"))]
-      (is (= out-str ":strong")))
-    ))
-
-(deftest test-dispatch
-  (testing "handles invalid args"
-    (let [data (dispatch '())]
-      (is (= (-> data :command) nil))
-      (is (= (-> data :problem) :no-command)))
-
-    (let [data (dispatch '("validate"))]
-      (is (= (-> data :command) :validate))
-      (is (= (-> data :problem) :no-arg)))
-
-    (let [data (dispatch '("murf" "mompl"))]
-      (is (= (-> data :command) "murf"))
-      (is (= (-> data :problem) :unknown-command)))
-
-    (let [data (dispatch '("generate" "this"))]
-      (is (= (-> data :command) :generate))
-      (is (= (-> data :problem) :too-many-args)))
-
-    (let [data (dispatch '("generate" "this" "more"))]
-      (is (= (-> data :command) :generate))
-      (is (= (-> data :problem) :too-many-args)))
-    )
-
-  (testing "handles valid args"
-    (let [data (dispatch '("validate" "mompl"))]
-      (is (= (-> data :command) :validate))
-      (is (= (-> data :problem) nil))
-      (is (= (-> data :result :rating) :strong)))
-
-    (let [data (dispatch '("generate"))]
-      (is (= (-> data :command) :generate))
-      (is (= (-> data :problem) nil))
-      (is (= (-> data :result :password) "password")))
-    ))
-
-
 (deftest test-validate
   (testing "rates passwords"
     (testing "strong"
@@ -105,3 +60,49 @@
         (is (= (-> (validate "abcdefghijklmno1987") :rating) :weak))
         (is (= (-> (validate "#XWVUTSRQPONMLKJIH$") :rating) :weak))
         ))))
+
+
+(deftest test-dispatch
+  (testing "handles invalid args"
+    (let [data (dispatch '())]
+      (is (= (-> data :command) nil))
+      (is (= (-> data :problem) :no-command)))
+
+    (let [data (dispatch '("validate"))]
+      (is (= (-> data :command) :validate))
+      (is (= (-> data :problem) :no-arg)))
+
+    (let [data (dispatch '("murf" "mompl"))]
+      (is (= (-> data :command) "murf"))
+      (is (= (-> data :problem) :unknown-command)))
+
+    (let [data (dispatch '("generate" "this"))]
+      (is (= (-> data :command) :generate))
+      (is (= (-> data :problem) :too-many-args)))
+
+    (let [data (dispatch '("generate" "this" "more"))]
+      (is (= (-> data :command) :generate))
+      (is (= (-> data :problem) :too-many-args)))
+    )
+
+  (testing "handles valid args"
+    (let [data (dispatch '("validate" "mompl"))]
+      (is (= (-> data :command) :validate))
+      (is (= (-> data :problem) nil))
+      (is (= (-> data :result :rating) :strong)))
+
+    (let [data (dispatch '("generate"))]
+      (is (= (-> data :command) :generate))
+      (is (= (-> data :problem) nil))
+      (is (= (-> data :result :password) "password")))
+    ))
+
+
+(deftest test-main
+  (testing "runs"
+    (let [out-str (with-out-str (-main))]
+      (is (= out-str "Invalid Command: No command given")))
+
+    (let [out-str (with-out-str (-main "validate" "mompl"))]
+      (is (= out-str ":strong")))
+    ))

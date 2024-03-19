261b384ebdd3e5b6f23a4fc43a752efe6dd78cfd
Podolski, Malte <malte.podolski@web.de>
2024-03-19 21:28:31 +0100

    pwt: add 'check-keyboard-patterns'

--- src/cloj/pw_tool/validation.clj
+++ src/cloj/pw_tool/validation.clj
@@ -14,16 +14,22 @@
                                                 {:lo 123 :up 126}]}
              :bad-surr-chars      [\$ \! \? \#]
              :min-len-repetitions 3
-             :pc-german           {:alphabetical ["qwertzuiopü" "asdfghjklöä"
-                                                  "<yxcvbnm,.-" ">YXCVBNM;:_"
-                                                  ]
-                                   :numerical    ["147" "258" "369" "159" "753"]
-                                   :special      ["!\"§$%&/()=?`" "{[]}\\~" ",.-#+"
-                                                  ";:_'*" "/*-+"]
-                                   :altgr        ["¹²³¼½¬{[]}\\¸{" "@ł€¶ŧ←↓→øþ¨~"
-                                                  "æſðđŋħ.ĸł" "|»«¢„“”µ·…–"
-                                                  "¡⅛'£¤⅜⅝⅞™±°¿˛" "ΩŁ€®Ŧ¥↑ıØÞ˚¯"
-                                                  "ÆẞÐªŊĦ˙&Ł" "ˍ‹©‚‘’º×÷—"]}})
+             :min-len-key-pat     5
+             :pc-german           {:alphabetical "abcdefghijklmnopqrstuvwxyz"
+                                   :upper-case   "!\"§$%&/()=?QWERTZUIOPÜ*ASDFGHJKLÖÄ>YXCVBNM;:_"
+                                   :lower-case   "1234567890ßqwertzuiopüasdfghjklöä<yxcvbnm,.-"
+                                   :special      "!\"§$%&/()=?`{[]}\\~,.-#+"
+                                   :altgr        "¹²³¼½¬{[]}\\¸{@ł€¶ŧ←↓→øþ¨~æſðđŋħ.ĸł|»«¢„“”µ·…–¡⅛'£¤⅜⅝⅞™±°¿˛ΩŁ€®Ŧ¥↑ıØÞ˚¯ÆẞÐªŊĦ˙&Łˍ‹©‚‘’º×÷—"}})
+
+
+(defn get-config-keys [config-key detect-fn password]
+  (reduce (fn [acc val]
+            (reduce #(conj %1 %2)
+                    acc
+                    (for [key (keys (-> config config-key))
+                          :when (detect-fn val (-> config config-key key))]
+                      key)))
+          #{} (map int password)))
 
 
 (defn in-range? [n bounds-list]
@@ -32,14 +38,8 @@
           (and (<= (-> bounds :lo) n) (>= (-> bounds :up) n)))))
 
 
-(defn get-char-classes [password]
-  (reduce (fn [acc ucp]
-            (reduce #(conj %1 %2)
-                    acc
-                    (for [range (keys (-> config :uc-ranges))
-                          :when (in-range? ucp (-> config :uc-ranges range))]
-                      range)))
-          #{} (map int password)))
+(def get-char-classes
+  (partial get-config-keys :uc-ranges in-range?))
 
 
 (defn- check-length-complexity [result]
@@ -62,16 +62,6 @@
                                                   :char-classes char-classes}}})))
 
 
-(defn- split-numbers-last [string]
-  (let [number-chars (take-while
-                       (fn [char] (-> char (str) (parse-long) (number?)))
-                       (reverse string))
-        number-seq-index (- (count string) (count number-chars))
-        rest (if (< number-seq-index (count string))
-               (subs string 0 number-seq-index) nil)]
-    [(str rest) (vec (reverse number-chars))]))
-
-
 (defn- is-number? [char]
   (cc/nnil? (parse-long (str char))))
 
@@ -85,6 +75,16 @@
 (defn- slice-last [string] (subs string 0 (- (count string) 1)))
 
 (def slice-both (comp slice-last slice-first))
+
+
+(defn- split-numbers-last [string]
+  (let [number-chars (take-while
+                       (fn [char] (-> char (str) (parse-long) (number?)))
+                       (reverse string))
+        number-seq-index (- (count string) (count number-chars))
+        rest (if (< number-seq-index (count string))
+               (subs string 0 number-seq-index) nil)]
+    [(str rest) (vec (reverse number-chars))]))
 
 
 (defn- slice-pw [password number-last bad-char-first bad-char-last]
@@ -127,20 +127,20 @@
                                                   :matches (-> sliced-pw :bad)}}})))
 
 
-(defn compare-segments [string size]
+(defn- compare-segments [string size]
   (let [pattern (take size string)]
     (reduce
       (fn [acc val]
-        (if (= (count pattern) (count val))
+        (if (= size (count val))
           (if (and (= pattern val) (true? acc)) true false)
           (if (and (= (take (count val) pattern) val) (true? acc)) true false)))
       true (partition-all size string))))
 
 
-(defn ceil-half [n] (if (even? n) (/ n 2) (/ (+ 1 n) 2)))
+(defn- ceil-half [n] (if (even? n) (/ n 2) (/ (+ 1 n) 2)))
 
 
-(defn find-repetitions [string min-length length]
+(defn- find-repetitions [string min-length length]
   (let [length (if (nil? length) (ceil-half (count string)) length)
         found-rep (compare-segments string length)
         pattern (subs string 0 length)]
@@ -157,17 +157,119 @@
 (defn- check-repeating-pattern [result]
   (let [{invalid :found-rep pattern :pattern}
         (find-repetitions (:password result) (:min-len-repetitions config) nil)
-        match (if (nil? pattern) [] [pattern])]
+        match (if-not (nil? pattern) [pattern])]
 
     (merge-with into result
                 {:validation {:repeating-pattern {:valid   (not invalid)
                                                   :matches match}}})))
 
 
+; -> password "abcde" -> (\b \c \d \e)
+; run predicate for each pattern -> boolean
+;   -> (\b \c \d \e) (\a \b \c ... \z)
+;   (*) find index of first char in pattern
+;   compare segment forwards and backwards from that index (cycle pattern)
+;   if match is shorter than password drop n chars and repeat (*)
+; if one true, return -> :category-key
+
+
+(defn- find-index [coll item] (-> (keep-indexed (fn [ind val] (if (= val item) ind)) coll)
+                                  (nth 0)))
+
+
+(defn- get-matching-items [result-list compare-list from]
+  (remove nil? (map #(if (= %1 %2) %1)
+                    compare-list
+                    (take (count compare-list) (drop from result-list)))))
+
+
+(defn- match-segment-to-pattern [segment pattern min-length]
+  (let [pattern-forward (cycle pattern)
+        pattern-backward (cycle (reverse pattern))
+        match-forward (get-matching-items
+                        pattern
+                        segment (find-index pattern-forward (first segment)))
+        match-backward (get-matching-items
+                         pattern
+                         segment (find-index pattern-backward (first segment)))]
+
+    (cond
+      (and (<= min-length (count match-forward)) (= segment match-forward))
+      [true match-forward]
+      (and (<= min-length (count match-backward)) (= segment match-backward))
+      [true match-backward]
+      :else [false])))
+
+;;'(\b \c \d \e \f)
+;;'(\o \n \m \l \k)
+;'(\o \n \m \l)
+;'(\a \b \c \d \e \f \g \h \i \j \k \l \m \n \o \p \q \r \s \t \u \v \w \x \y \z)
+;5
+
+
+
+((fn compare-segments-with-keyboard-pattern [string size pattern]
+   (let [
+         ;chunk (take size string)
+         ]
+     (reduce
+       (fn [acc val]
+         ;(if (= size (count val))
+         ;  (if (and (= chunk val) (true? acc)) true false)
+         ;  (if (and (= (take (count val) chunk) val) (true? acc)) true false))
+         (if ((fn keyboard-pattern? [segment pattern] true) val (seq pattern)) (conj acc val))
+         ;acc
+         ;(println val)
+         )
+       #{} (partition-all size string)
+       )
+     )
+   )
+ "bcdefxx" 5 "abcdefghijklmnopqrstuvwxyz"
+ )
+
+
+(defn- has-pattern? [string pattern-list] [true "pattern"])
+
+((fn find-keyboard-pattern [string pattern-list min-length length]
+   (let [
+         pattern "value"
+         ]
+     [true pattern]
+     )
+   )
+ "abcdexxx" ["abcdefghijklmnopqrstuvwxyz" "qwertzuiopü"] 5 nil
+ )
+
+;(def get-keyboard-pattern-classes
+;  (partial get-config-keys ((:keyboard-layout config) config) has-pattern?))
+
+
+(defn get-keyboard-pattern-classes [config-key detect-fn password]
+  (reduce (fn [acc val]
+            (reduce #(conj %1 %2)
+                    acc
+                    (for [key (keys (-> config config-key))
+                          :when (-> (detect-fn val (-> config config-key key))
+                                    (nth 0))]
+                      key)))
+          #{} (map int password)))
+
+(get-keyboard-pattern-classes :pc-german has-pattern? "abcde")
+
+
 (defn- check-keyboard-patterns [result]
-  (merge-with into result
-              {:validation {:keyboard-patterns {:valid   true
-                                                :matches []}}}))
+  (let [
+        {invalid :found-pat pattern :pattern}
+        (get-keyboard-pattern-classes
+          ((:keyboard-layout config) config) has-pattern? (:password result))
+        match (if-not (nil? pattern) [pattern])
+        ]
+    (merge-with into result
+                {:validation {:keyboard-patterns {:valid   (not invalid)
+                                                  :matches [match]}}})
+    )
+  )
 
 
 (defn- rate [result]
